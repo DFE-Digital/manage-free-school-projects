b6c3ac71a5b4c23d5f55d3e02f7a11d0cccbcd98
AZIA, Stephane <Stephane.AZIA@EDUCATION.GOV.UK>
2025-10-22 12:05:04 +0100

    webpack error

--- Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects/wwwroot/webpack.assets.config.js
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects/wwwroot/webpack.assets.config.js
@@ -6,7 +6,7 @@ module.exports = {
 	plugins: [
 		new CopyPlugin({
 			patterns: [
-				{ from: path.joresolvein(__dirname, 'node_modules/govuk-frontend/dist/govuk/assets'), to: path.resolve(__dirname, 'assets') },
+				{ from: path.resolve(__dirname, 'node_modules/govuk-frontend/dist/govuk/assets'), to: path.resolve(__dirname, 'assets') },
 				{ from: path.resolve(__dirname, 'node_modules/accessible-autocomplete/dist'), to: path.resolve(__dirname, 'dist') },
 				{ from: path.resolve(__dirname, 'node_modules/jquery/dist'), to: path.resolve(__dirname, 'dist') },
 			],
