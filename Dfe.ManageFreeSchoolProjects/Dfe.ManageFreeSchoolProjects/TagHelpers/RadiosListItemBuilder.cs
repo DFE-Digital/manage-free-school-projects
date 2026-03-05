9f63d4bedf2dd2753d0a04a1bcd41b34422a3786
AREMU, Elijah <Elijah.AREMU@EDUCATION.GOV.UK>
2026-03-05 20:10:13 +0000

    readd after merge main

--- Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects/TagHelpers/RadiosListItemBuilder.cs
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects/TagHelpers/RadiosListItemBuilder.cs
@@ -17,12 +17,17 @@ internal static class RadiosListItemBuilder
                      </label>";
     }
 
-    public static string BuildRadioInput(string id, string value, ModelExpression aspfor, string name = null, string conditionallink = null, bool hasHint = false)
+    public static string BuildRadioInput(string id, string value, ModelExpression aspfor, string name = null, string conditionallink = null, bool hasHint = false, string additionalAriaDescribedBy = null)
     {
         var radioChecked = value == aspfor.Model?.ToString() ? "checked=\"checked\"" : "";
         var conditional = conditionallink != null ? $@"data-aria-controls=""{conditionallink}""" : "";
         var nameAttributeAndValue = !string.IsNullOrEmpty(name) ? $@"name=""{name}""" : ""; 
-        var ariaDescribedBy = hasHint ? $@"aria-describedby=""{id.ToLower()}-hint""" : "";
+        
+        var ariaDescribedByList = new System.Collections.Generic.List<string>();
+        if (hasHint) ariaDescribedByList.Add($"{id.ToLower()}-hint");
+        if (!string.IsNullOrEmpty(additionalAriaDescribedBy)) ariaDescribedByList.Add(additionalAriaDescribedBy);
+        
+        var ariaDescribedBy = ariaDescribedByList.Count > 0 ? $@"aria-describedby=""{string.Join(" ", ariaDescribedByList)}""" : "";
         
         return $@"<input class=""govuk-radios__input"" id=""{id}""  type=""radio"" value=""{value}"" {nameAttributeAndValue} {radioChecked} {conditional} {ariaDescribedBy}/>";
     }
