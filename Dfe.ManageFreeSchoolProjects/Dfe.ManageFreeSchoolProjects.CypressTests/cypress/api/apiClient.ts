fa0e1dc4800cd9d4a81277715bf4f39a63009747
paddy-clark <patrick.clark@education.gov.uk>
2026-03-24 12:07:34 +0000

    update apiclient

--- Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.CypressTests/cypress/api/apiClient.ts
+++ Dfe.ManageFreeSchoolProjects/Dfe.ManageFreeSchoolProjects.CypressTests/cypress/api/apiClient.ts
@@ -5,52 +5,56 @@ export class ApiClient {
         endpoint: string,
         request: TRequest
     ): Cypress.Chainable<TResponse> {
-        return cy
-            .request<TResponse>({
-                method: 'PUT',
-                url: Cypress.expose(EnvApi) + endpoint,
-                headers: this.getHeaders(),
-                body: request,
-            })
-            .then((response) => {
-                return response.body;
-            });
+        return cy.env([EnvApiKey]).then(({ apiKey }) => {
+            return cy
+                .request<TResponse>({
+                    method: 'PUT',
+                    url: Cypress.expose(EnvApi) + endpoint,
+                    headers: this.getHeaders(apiKey),
+                    body: request,
+                })
+                .then((response) => {
+                    return response.body;
+                });
+        });
     }
 
     public post<TRequest extends object, TResponse extends object>(
         endpoint: string,
         request: TRequest
     ): Cypress.Chainable<TResponse> {
-        return cy
-            .request<TResponse>({
-                method: 'POST',
-                url: Cypress.expose(EnvApi) + endpoint,
-                headers: this.getHeaders(),
-                body: request,
-            })
-            .then((response) => {
-                return response.body;
-            });
+        return cy.env([EnvApiKey]).then(({ apiKey }) => {
+            return cy
+                .request<TResponse>({
+                    method: 'POST',
+                    url: Cypress.expose(EnvApi) + endpoint,
+                    headers: this.getHeaders(apiKey),
+                    body: request,
+                })
+                .then((response) => {
+                    return response.body;
+                });
+        });
     }
 
     public get<TResponse extends object>(endpoint: string): Cypress.Chainable<TResponse> {
-        return cy
-            .request<TResponse>({
-                method: 'GET',
-                url: Cypress.expose(EnvApi) + endpoint,
-                headers: this.getHeaders(),
-            })
-            .then((response) => {
-                return response.body;
-            });
+        return cy.env([EnvApiKey]).then(({ apiKey }) => {
+            return cy
+                .request<TResponse>({
+                    method: 'GET',
+                    url: Cypress.expose(EnvApi) + endpoint,
+                    headers: this.getHeaders(apiKey),
+                })
+                .then((response) => {
+                    return response.body;
+                });
+        });
     }
 
-    protected getHeaders(): object {
-        return cy.env([EnvApiKey]).then(({ apiKey }) => {
-            return {
-                'Content-type': 'application/json',
-                ApiKey: apiKey,
-            };
-        });
+    protected getHeaders(apiKey: string): object {
+        return {
+            'Content-type': 'application/json',
+            ApiKey: apiKey,
+        };
     }
 }
